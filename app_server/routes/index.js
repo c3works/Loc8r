47eb5173f1e890a19fdfc485dd29536a86e7332c
c3works <wlaprise@gmail.com>
2015-09-08 06:25:00 -0500

    angular homepage with routing works

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,19 +1,20 @@
+// ** Modified and commented to switch from express to Angularjs routing
+
 var express =       require('express');
 var router =        express.Router();
 
-var locationCtrl =  require('../controllers/locations');
+//var locationCtrl =  require('../controllers/locations');
 var otherCtrl =     require('../controllers/other');
 
 //  location pages
 
-router.get('/', locationCtrl.homelist);
-router.get('/location/:locationid', locationCtrl.locationInfo);
-router.get('/location/:locationid/review/new', locationCtrl.addReview);
-
-router.post('/location/:locationid/review/new', locationCtrl.doAddReview);
+router.get('/', otherCtrl.angularApp); // from locationCtrl.locationInfo
+//router.get('/location/:locationid', locationCtrl.locationInfo);
+//router.get('/location/:locationid/review/new', locationCtrl.addReview);
+//router.post('/location/:locationid/review/new', locationCtrl.doAddReview);
 
 //  other pages
 
-router.get('/about', otherCtrl.about);
+//router.get('/about', otherCtrl.about);
 
 module.exports = router;
