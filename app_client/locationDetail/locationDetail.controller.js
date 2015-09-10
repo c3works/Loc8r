47569c98e81b612682d0ab04394e14108909b898
c3works <wlaprise@gmail.com>
2015-09-10 14:31:53 -0500

    locationDetail controller shows id in title

---
+++ app_client/locationDetail/locationDetail.controller.js
@@ -0,0 +1,15 @@
+(function() {
+    angular
+        .module('loc8rApp')
+        .controller('locationDetailCtrl', locationDetailCtrl);
+
+    locationDetailCtrl.$inject = ['$routeParams'];
+    function locationDetailCtrl ($routeParams){
+        var vm = this;
+        vm.locationid = $routeParams.locationid;
+
+        vm.pageHeader = {
+            title: vm.locationid
+        }
+    }
+})();
