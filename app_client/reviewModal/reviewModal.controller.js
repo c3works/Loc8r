cf35cd917e8d383a2051968891740c94045f9e80
wlaprise <wlaprise@gmail.com>
2015-09-12 19:06:23 -0500

    validation 1 no errors

--- app_client/reviewModal/reviewModal.controller.js
+++ app_client/reviewModal/reviewModal.controller.js
@@ -9,6 +9,16 @@
         var vm = this;
         vm.locationData = locationData;
 
+        vm.onSubmit = function(){
+          vm.formError = "";
+            if(!vm.formData.name || !vm.formData.reviewText) {
+                vm.formError = "Fill in the FIELDS";
+                return false;
+            } else {
+                console.log("*****>> " + formData.name + " | " + formData.reviewText);
+            }
+        };
+
         vm.modal = {
         cancel : function () {
             $modalInstance.dismiss('cancel');
